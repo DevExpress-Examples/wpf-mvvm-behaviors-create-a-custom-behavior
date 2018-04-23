b080d1162923562e6cf19b0554c49580f1b3d81c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Create a Custom Attached Behavior
+
+
+<p>This example demonstrates how to create a custom attached behavior (a Behavior descendant).<br />Review <a href="https://documentation.devexpress.com/#WPF/CustomDocument17458">this documentation topic</a> to learn more.</p>
+
+<br/>
+
+
---
+++ VB/ViewModel/MainViewModel.vb
@@ -0,0 +1,37 @@
+﻿Imports DevExpress.Mvvm.DataAnnotations
+Imports System.Windows.Media
+
+Namespace Example.ViewModel
+    <POCOViewModel> _
+    Public Class MainViewModel
+        Public Overridable Property InvalidValue() As String
+        Public Overridable Property ValidBrush() As Brush
+        Public Overridable Property InvalidBrush() As Brush
+
+        Public Sub SetRedValidBrush()
+            ValidBrush = New SolidColorBrush(Colors.Red)
+        End Sub
+        Public Sub SetBlueValidBrush()
+            ValidBrush = New SolidColorBrush(Colors.Blue)
+        End Sub
+        Public Sub SetBlackValidBrush()
+            ValidBrush = New SolidColorBrush(Colors.Black)
+        End Sub
+
+        Public Sub SetRedInvalidBrush()
+            InvalidBrush = New SolidColorBrush(Colors.Red)
+        End Sub
+        Public Sub SetBlueInvalidBrush()
+            InvalidBrush = New SolidColorBrush(Colors.Blue)
+        End Sub
+        Public Sub SetBlackInvalidBrush()
+            InvalidBrush = New SolidColorBrush(Colors.Black)
+        End Sub
+
+        Public Sub New()
+            InvalidValue = "Error"
+            SetBlackValidBrush()
+            SetRedInvalidBrush()
+        End Sub
+    End Class
+End Namespace
