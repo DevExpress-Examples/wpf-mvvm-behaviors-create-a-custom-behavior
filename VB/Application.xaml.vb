63dc981a638c5c5486541e42ebfad4f0799a3a68
DevExpressExampleBot <fakeEmail>
2021-09-06 20:54:42 +0300

    autogenerate VB

--- VB/Application.xaml.vb
+++ VB/Application.xaml.vb
@@ -1,22 +1,22 @@
 ﻿Imports System.Windows
 
 Namespace Example
-    Partial Public Class App
-        Inherits Application
+	Partial Public Class App
+		Inherits Application
 
-        Public Sub New()
-            AddHandler Startup, AddressOf OnStartup
+		Public Sub New()
+			AddHandler Me.Startup, AddressOf OnStartup
 #If SILVERLIGHT Then
-            InitializeComponent()
+			InitializeComponent()
 #End If
-        End Sub
-        Private Overloads Sub OnStartup(ByVal sender As Object, ByVal e As StartupEventArgs)
+		End Sub
+		Private Overloads Sub OnStartup(ByVal sender As Object, ByVal e As StartupEventArgs)
 #If SILVERLIGHT Then
-            Me.RootVisual = New MainPage()
+			Me.RootVisual = New MainPage()
 #Else
-            MainWindow = New MainWindow()
-            MainWindow.Show()
+			MainWindow = New MainWindow()
+			MainWindow.Show()
 #End If
-        End Sub
-    End Class
+		End Sub
+	End Class
 End Namespace
--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -2,36 +2,36 @@
 Imports System.Windows.Media
 
 Namespace Example.ViewModel
-    <POCOViewModel> _
-    Public Class MainViewModel
-        Public Overridable Property InvalidValue() As String
-        Public Overridable Property ValidBrush() As Brush
-        Public Overridable Property InvalidBrush() As Brush
+	<POCOViewModel>
+	Public Class MainViewModel
+		Public Overridable Property InvalidValue() As String
+		Public Overridable Property ValidBrush() As Brush
+		Public Overridable Property InvalidBrush() As Brush
 
-        Public Sub SetRedValidBrush()
-            ValidBrush = New SolidColorBrush(Colors.Red)
-        End Sub
-        Public Sub SetBlueValidBrush()
-            ValidBrush = New SolidColorBrush(Colors.Blue)
-        End Sub
-        Public Sub SetBlackValidBrush()
-            ValidBrush = New SolidColorBrush(Colors.Black)
-        End Sub
+		Public Sub SetRedValidBrush()
+			ValidBrush = New SolidColorBrush(Colors.Red)
+		End Sub
+		Public Sub SetBlueValidBrush()
+			ValidBrush = New SolidColorBrush(Colors.Blue)
+		End Sub
+		Public Sub SetBlackValidBrush()
+			ValidBrush = New SolidColorBrush(Colors.Black)
+		End Sub
 
-        Public Sub SetRedInvalidBrush()
-            InvalidBrush = New SolidColorBrush(Colors.Red)
-        End Sub
-        Public Sub SetBlueInvalidBrush()
-            InvalidBrush = New SolidColorBrush(Colors.Blue)
-        End Sub
-        Public Sub SetBlackInvalidBrush()
-            InvalidBrush = New SolidColorBrush(Colors.Black)
-        End Sub
+		Public Sub SetRedInvalidBrush()
+			InvalidBrush = New SolidColorBrush(Colors.Red)
+		End Sub
+		Public Sub SetBlueInvalidBrush()
+			InvalidBrush = New SolidColorBrush(Colors.Blue)
+		End Sub
+		Public Sub SetBlackInvalidBrush()
+			InvalidBrush = New SolidColorBrush(Colors.Black)
+		End Sub
 
-        Public Sub New()
-            InvalidValue = "Error"
-            SetBlackValidBrush()
-            SetRedInvalidBrush()
-        End Sub
-    End Class
+		Public Sub New()
+			InvalidValue = "Error"
+			SetBlackValidBrush()
+			SetRedInvalidBrush()
+		End Sub
+	End Class
 End Namespace
